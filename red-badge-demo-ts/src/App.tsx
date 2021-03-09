de71d53ac0036e71ca55d58ab85f06e0eb951439
Rob Vanarsdall <rvanar@gmail.com>
2021-03-08 20:49:52 -0500

    added challenge 01

--- red-badge-demo-ts/src/App.tsx
+++ red-badge-demo-ts/src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <Container>
+        <h2>Convert to Class Challenge 01</h2>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Take convert the the component (InfoCard.tsx) and turn them into class
+          components in a new file (InfoCardClass.tsx) and bring them in to the
+          App.tsx file. Please use a different picutre in each mount of
+          InfoCardClass
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Row>
+          <Col md="4">{/* INFOCARDCLASS HERE */}</Col>
+          <Col md="4">{/* INFOCARDCLASS HERE */}</Col>
+        </Row>
+      </Container>
     </div>
   );
 }
