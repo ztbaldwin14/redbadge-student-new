5dd4e20185de0179bf7ed3c9f674864738829583
Rob Vanarsdall <rvanar@gmail.com>
2021-03-14 18:31:24 -0400

    added challenge words

--- red-badge-demo-ts/src/App.tsx
+++ red-badge-demo-ts/src/App.tsx
@@ -1,7 +1,38 @@
 import React from "react";
+import { Container } from "reactstrap";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Container>
+        <h2>Challenge 2</h2>
+        <p>In this challenge you will conduct a fetch from AdviceSlip's API</p>
+        <ul>
+          <li>
+            Website Url for Docs:
+            <a
+              href="https://api.adviceslip.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              https://api.adviceslip.com/
+            </a>
+          </li>
+          <li>API you will use: https://api.adviceslip.com/advice </li>
+          <li>Create the AdviceSlip.tsx file</li>
+          <li>The fetch will happen with the Onload of the page.</li>
+          <li>Class component will be used</li>
+          <li>
+            App.tsx will be passing the api url down as a prop for the practice
+            of passing props.
+          </li>
+          <li>Use Interfaces</li>
+          <li>Display the Advice</li>
+        </ul>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
