2a3287c0d4e90a1041571b000420b423324914dd
Rob Vanarsdall <rvanar@gmail.com>
2021-03-14 18:33:07 -0400

    fixed challenge prompt

--- red-badge-demo-ts/src/App.tsx
+++ red-badge-demo-ts/src/App.tsx
@@ -1,28 +1,34 @@
 import React from "react";
-import "./App.css";
 import { Container } from "reactstrap";
 
 function App() {
-  const url = "https://api.adviceslip.com/advice";
   return (
     <div>
       <Container>
         <h2>Challenge 2</h2>
-        <p>
-          For this challenge you will need to convert the functional component
-          and convert it into a class component. The fetch will need to be
-          completed on page load - no button clicks.
-        </p>
+        <p>In this challenge you will conduct a fetch from AdviceSlip's API</p>
         <ul>
           <li>
-            Create an iterface in the functional component and class component.
-            Make the interface based on the response you get back from the API.
+            Website Url for Docs:
+            <a
+              href="https://api.adviceslip.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              https://api.adviceslip.com/
+            </a>
           </li>
+          <li>API you will use: https://api.adviceslip.com/advice </li>
+          <li>Create the AdviceSlip.tsx file</li>
+          <li>The fetch will happen with the Onload of the page.</li>
+          <li>Class component will be used</li>
           <li>
-            Use the url in this file to pass down as a prop to use in the
-            conponent
+            App.tsx will be passing the api url down as a prop for the practice
+            of passing props.
           </li>
-          <li>Conduct the fetch on page load</li>
+          <li>Use Interfaces</li>
+          <li>Display the Advice</li>
         </ul>
       </Container>
     </div>
