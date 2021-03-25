b9f45fd7a680a8252fb021b9f89ea02488740977
ztbaldwin14 <ztbaldwin14@gmail.com>
2021-03-25 19:34:11 -0400

    challenge04 initial

--- red-badge-demo-ts/src/App.tsx
+++ red-badge-demo-ts/src/App.tsx
@@ -1,12 +1,16 @@
 import React from "react";
-import FunctionalClickCounter from "./component/FunctionalClickCounter";
+import FunctionalClickCounter from "./components/FunctionalClickCounter";
+import ClassClickCounter from "./components/ClassClickCounter";
 
 function App() {
   return (
     <div>
-      <FunctionalClickCounter initialValue={6} />
+      {/* <FunctionalClickCounter initialValue={6} />
       <FunctionalClickCounter initialValue={10} />
-      <FunctionalClickCounter initialValue={4} />
+      <FunctionalClickCounter initialValue={4} /> */}
+      <ClassClickCounter initialValue={6} />
+      <ClassClickCounter initialValue={10} />
+      <ClassClickCounter initialValue={4} />
     </div>
   );
 }
--- red-badge-demo-ts/src/components/ClassClickCounter.jsx
+++ red-badge-demo-ts/src/components/ClassClickCounter.jsx
@@ -1,1 +1,23 @@
 // Create a class version of the Functional Component
+import React, { Component } from "react";
+import { Button, Container } from "reactstrap";
+
+class ClassClickCounter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { countValue: this.props.initialValue };
+  }
+  handleClick = () => {
+    this.setState({ countValue: this.state.countValue - 1 });
+  };
+  render() {
+    return (
+      <Container>
+        <p>The total clicks left are {this.state.countValue}</p>
+        <Button onClick={this.handleClick}> Click Me From Function</Button>
+      </Container>
+    );
+  }
+}
+
+export default ClassClickCounter;
